b3c73bdf37a409488fe2cfcf46e7a8e9c22f3c2f
CCP101 <liwentao0523@hotmail.com>
2018-04-28 15:58:28 +0800

    Python Learning
    learning chapter 2
    
    Time:2018/4/28
    Location:dormitory

--- Python_Learning_Code/Python_Learning_Code/chapter_02/hello_world.py
+++ Python_Learning_Code/Python_Learning_Code/chapter_02/hello_world.py
@@ -3,3 +3,6 @@ print(message)
 
 message = "Hello Python Crash Course world!"
 print(message)
+
+# there is one thing you should consider about when you name things
+# try your best do not use 小写字母l和大写字母O，因为它们可能被人错看成数字1和0
--- Python_Learning_Code/Python_Learning_Code/chapter_02/name.py
+++ Python_Learning_Code/Python_Learning_Code/chapter_02/name.py
@@ -4,3 +4,9 @@ full_name = first_name + " " + last_name
 
 message = "Hello, " + full_name.title() + "!"
 print(message)
+
+##python rule like this you can use "" or '' at the same time
+
+'I told my friend, "Python is my favorite language!"'
+"The language 'Python' is named after Monty Python, not the snake."
+"One of Python's strengths is its diverse and supportive community."
