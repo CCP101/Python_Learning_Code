d24b4cc3f554c9b4df97f4bada4011a13fb8e56a
CCP101 <liwentao0523@hotmail.com>
2018-07-16 23:08:46 +0800

    Python Learning
    learning chapter 12 to page 210
    
    Time:2018/7/16
    Location:home

--- Python教材代码/Python编程从入门到实战/chapter_12/restore_points/restore_point_1_ship_moves/alien_invasion.py
+++ Python教材代码/Python编程从入门到实战/chapter_12/restore_points/restore_point_1_ship_moves/alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+
 def run_game():
     # Initialize pygame, settings, and screen object.
     pygame.init()
@@ -11,17 +12,18 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    
+
     # Set the background color.
     bg_color = (230, 230, 230)
-    
+
     # Make a ship.
     ship = Ship(ai_settings, screen)
-    
+
     # Start the main loop for the game.
     while True:
         gf.check_events(ship)
         ship.update()
         gf.update_screen(ai_settings, screen, ship)
-        
+
+
 run_game()
