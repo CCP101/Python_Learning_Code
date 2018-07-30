ee21aa21829bccf1ebaca6eb0ae6ed3fccd434a7
CCP101 <liwentao0523@hotmail.com>
2018-07-30 21:56:59 +0800

    Python Learning
    learning chapter 15 to page 312
    
    Time:2018/7/30
    Location:home

--- Python教材代码/Python编程从入门到实战/chapter_15/scatter_squares.py
+++ Python教材代码/Python编程从入门到实战/chapter_15/scatter_squares.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+plt.scatter(x_values, y_values, c=(0, 0, 1), edgecolor='none', s=10)
 
 # Set chart title, and label axes.
-plt.title("Square Numbers", fontsize=24)
+plt.title("Square Numbers", fontsize=14)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
